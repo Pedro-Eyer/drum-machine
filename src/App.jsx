0a0825a309f645352f0c4c02d7d5789a70ce2ea3
Pedro Eyer <pedrojulioeyr@gmail.com>
2025-08-01 20:48:01 -0300

    adiciona URLs de áudio aos pads de tambores no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./App.css";
 
 const drumPads = [
-  { key: "Q", sound: "Heater 1" },
-  { key: "W", sound: "Heater 2" },
-  { key: "E", sound: "Heater 3" },
-  { key: "A", sound: "Heater 4" },
-  { key: "S", sound: "Clap" },
-  { key: "D", sound: "Open-HH" },
-  { key: "Z", sound: "Kick-n'-Hat" },
-  { key: "X", sound: "Kick" },
-  { key: "C", sound: "Closed-HH" },
+  { key: "Q", sound: "Heater 1", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "W", sound: "Heater 2", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "E", sound: "Heater 3", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"},
+  { key: "A", sound: "Heater 4", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"},
+  { key: "S", sound: "Clap", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "D", sound: "Open-HH", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "Z", sound: "Kick-n'-Hat", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "X", sound: "Kick", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "C", sound: "Closed-HH", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
 ]
 function App() {
   return (
