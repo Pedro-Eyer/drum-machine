226f49f1110d4a35b99a70a60236e3857e75f1e9
Pedro Eyer <pedrojulioeyr@gmail.com>
2025-08-01 17:08:19 -0300

    adiciona botões de tambores ao componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./App.css";
 
+const drumPads = [
+  { key: "Q", sound: "Heater 1" },
+  { key: "W", sound: "Heater 2" },
+  { key: "E", sound: "Heater 3" },
+  { key: "A", sound: "Heater 4" },
+  { key: "S", sound: "Clap" },
+  { key: "D", sound: "Open-HH" },
+  { key: "Z", sound: "Kick-n'-Hat" },
+  { key: "X", sound: "Kick" },
+  { key: "C", sound: "Closed-HH" },
+]
 function App() {
   return (
     <div id="drum-machine">
@@ -8,6 +19,15 @@ function App() {
       <div id="display">
         {/* Este será o display onde mostraremos o nome do som tocado */}
         Pressione um botão
+      </div>
+
+      {/* Botões de tambores */}
+      <div className="drum-pads">
+        {drumPads.map((pad) => (
+          <div key={pad.key} id={pad.sound} className="drum-pad">
+            {pad.key}
+          </div>
+        ))}
       </div>
     </div>
   );
