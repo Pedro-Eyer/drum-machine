94e7ae31aeca4bd6a50079bd4a536263f404112e
Pedro Eyer <pedrojulioeyr@gmail.com>
2025-08-02 17:54:08 -0300

    atualiza URLs de áudio dos pads de tambores no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./App.css";
 
 const drumPads = [
   { key: "Q", sound: "Heater 1", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
-  { key: "W", sound: "Heater 2", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
-  { key: "E", sound: "Heater 3", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"},
-  { key: "A", sound: "Heater 4", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"},
-  { key: "S", sound: "Clap", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
-  { key: "D", sound: "Open-HH", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
-  { key: "Z", sound: "Kick-n'-Hat", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
-  { key: "X", sound: "Kick", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
-  { key: "C", sound: "Closed-HH", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" },
+  { key: "W", sound: "Heater 2", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3" },
+  { key: "E", sound: "Heater 3", url:"https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3" },
+  { key: "A", sound: "Heater 4", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3" },
+  { key: "S", sound: "Clap", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3" },
+  { key: "D", sound: "Open-HH", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3" },
+  { key: "Z", sound: "Kick-n'-Hat", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3" },
+  { key: "X", sound: "Kick", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3" },
+  { key: "C", sound: "Closed-HH", url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3" },
 ]
 function App() {
   return (
@@ -26,6 +26,7 @@ function App() {
         {drumPads.map((pad) => (
           <div key={pad.key} id={pad.sound} className="drum-pad">
             {pad.key}
+            <audio className="clip" id={pad.key} src={pad.url}></audio>
           </div>
         ))}
       </div>
